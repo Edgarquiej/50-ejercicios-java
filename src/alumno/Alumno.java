b03f87304321fd96ff4c0ca05f05743f69cbe539
Ramirez <Ramirez@192.168.137.1>
2018-08-16 13:05:45 -0600

    mi proyecto de alumno.java

---
+++ src/alumno/Alumno.java
@@ -0,0 +1,16 @@
+//esutiantes
+package alumno;
+
+//completar
+public class Alumno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Univercidad a = new Univercidad();
+        a.setVisible(true);
+    }
+    
+}
